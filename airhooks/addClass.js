b326ab8656cffaf7e78b99c196665996411a9b8c
Jasper Moelker <j.b.moelker@activeids.nl>
2014-05-29 16:26:46 +0200

    add airhook: addClass

---
+++ airhooks/addClass.js
@@ -0,0 +1,31 @@
+/**
+ * @module airhooks/addClass OR
+ * @method airhooks.addClass
+ *
+ * Adds a class to an element's list of classes.
+ * [MDN docs](https://developer.mozilla.org/en-US/docs/Web/API/Element.classList)
+ * [Original source](http://hacks.mozilla.org/2010/01/classlist-in-firefox-3-6/)
+ *
+ * @param {HtmlElement} element
+ * @param {String} className
+ */
+(function (root, airhooks, factory) {
+	var method = 'addClass';
+	if (typeof define === 'function' && define.amd) {
+		define([/*deps*/], factory);
+	} else {
+		airhooks[method] = factory(/*deps*/);
+		root.airhooks = airhooks;
+	}
+}(this, this.airhooks || {}, function (/*deps*/) {
+	return function (element, className) {
+		if (document.documentElement.classList) {
+			element.classList.add(className);
+		} else {
+			if (!containsClass(element, className)) {
+				element.className += (element.className ? ' ' : '') + className;
+			}
+		}
+	};
+}));
+
